165e29721a408bb88fa755257a6218894a26f839
apkharsh@gmail.com <apkharsh@gmail.com>
2023-04-19 17:26:27 +0530

    booking form connected to backend

--- src/Pages/Booking/BookNow.js
+++ src/Pages/Booking/BookNow.js
@@ -2,14 +2,86 @@ import React, { useState } from "react";
 import avatar from "../../Assets/avatar.png";
 
 export default function BookNow() {
+
   const [data, setData] = useState({
     username: "",
     email: "",
     roomType: "",
-    roomNumber: "",
-    startTime: "",
-    endTime: "",
+    roomNumber: null,
+    startTime: 0,
+    endTime: 0,
   });
+
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // user will enter the date in normal format but backend will only handle unix timestamp
+    // so convert the date to unix timestamp
+
+    if (name === "startTime") {
+      const unixTime = new Date(value).getTime() / 1000;
+      setData((prevData) => {
+        return {
+          ...prevData,
+          startTime: unixTime,
+        };
+      });
+    }
+
+    else if (name === "endTime") {
+      const unixTime = new Date(value).getTime() / 1000;
+      setData((prevData) => {
+        return {
+          ...prevData,
+          endTime: unixTime,
+        };
+      });
+    }
+
+    else{
+      setData((prevData) => {
+        return {
+          ...prevData,
+          [name]: value,
+        };
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { username, email, roomType, startTime, endTime } = data;
+    // send a post request to localhost:5000/api/bookings/create
+    // with data as body
+
+    fetch("http://localhost:5000/api/bookings/create", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        // use cors
+        // "Cors": "no-cors",
+        // "Access-Control-Allow-Origin": "*",
+        "xFormUrlEncoded": "true",
+      },
+      body: JSON.stringify({username, email, roomType, startTime, endTime})
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  // use axios instead of fetch
+  // axios.post("http://localhost:5000/api/bookings/create", data)
+    
+  //   .then((res) => console.log(res))
+  //   .catch((err) => console.log(err));
+  // };
+
+
+
 
   return (
     <form>
@@ -40,6 +112,7 @@ export default function BookNow() {
               <input
                 type="text"
                 name="username"
+                onChange={handleChange}
                 placeholder="ApkHarsh"
                 className="outline-none w-full px-2 py-3 border rounded-md shadow focus:shadow-lg transition-all"
               />
@@ -53,6 +126,7 @@ export default function BookNow() {
               <input
                 type="email"
                 name="email"
+                onChange={handleChange}
                 placeholder="harsh.kumar@gmail.com"
                 className="outline-none w-full px-2 py-3 border rounded-md shadow focus:shadow-lg transition-all"
               />
@@ -66,7 +140,8 @@ export default function BookNow() {
                 </label>
                 <input
                   type="text"
-                  name="username"
+                  name="roomNumber"
+                  onChange={handleChange}
                   placeholder="Username"
                   className="outline-none w-full px-2 py-3 border rounded-md shadow focus:shadow-lg transition-all"
                 />
@@ -77,7 +152,8 @@ export default function BookNow() {
                 </label>
                 <input
                   type="text"
-                  name="username"
+                  name="roomType"
+                  onChange={handleChange}
                   placeholder="Username"
                   className="outline-none w-full px-2 py-3 border rounded-md shadow focus:shadow-lg transition-all"
                 />
@@ -90,18 +166,20 @@ export default function BookNow() {
             <div className="rounded-md border px-4 py-5 w-full h-full bg-white flex flex-col justify-between shadow hover:shadow-lg transition-all ease-linear">
               <input
                 type="datetime-local"
-                name="date"
+                name="startTime"
                 id=""
                 className="text-2xl font-bold outline-none"
+                onChange={handleChange}
               />
               <p className="text-[16px] text-gray-600"> Check-in </p>
             </div>
             <div className="rounded-md border px-4 py-5 w-full h-full bg-white flex flex-col justify-between shadow hover:shadow-lg transition-all ease-linear">
               <input
                 type="datetime-local"
-                name="date"
+                name="endTime"
                 id=""
                 className="text-2xl font-bold outline-none"
+                onChange={handleChange}
               />
               <p className="text-[16px] text-gray-600"> Check-out </p>
             </div>
@@ -110,7 +188,7 @@ export default function BookNow() {
       </div>
 
       <div className="flex item-center justify-end mt-5">
-        <button className="px-2 w-52 py-3 rounded-md bg-p4 text-white hover:bg-red-500 hover:shadow-xl transition-all">
+        <button onClick={handleSubmit} className="px-2 w-52 py-3 rounded-md bg-p4 text-white hover:bg-red-500 hover:shadow-xl transition-all">
           Book
         </button>
       </div>

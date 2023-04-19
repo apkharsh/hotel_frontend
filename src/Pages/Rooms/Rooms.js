d76e559ff442337651b60c6ae37bf20280a34874
w-udhav <udhavwadhawan@hotmail.com>
2023-04-19 17:59:54 +0530

    update: rooms are responsive

--- src/Pages/Rooms/Rooms.js
+++ src/Pages/Rooms/Rooms.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import bg from "../../Assets/bg.jpg";
 import Modal from "./Modal";
 import { CloseIcon, CubeIcon } from "../../Components/Icons";
@@ -17,11 +17,11 @@ export default function Rooms() {
     const res = await fetch("http://localhost:5000/api/rooms/all");
     const data = await res.json();
     setRooms(data.rooms);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getAllRooms();
-  },[])
+  }, []);
 
   return (
     <motion.div
@@ -50,30 +50,34 @@ export default function Rooms() {
 
       <div className="border-t border-gray-400"></div>
 
-      {rooms.map((room) => {
-        return (
-        <div className="h-full flex gap-5">
-        <div className="min-w-[13rem] max-w-[14rem] max-h-[13rem] relative shadow-md hover:shadow-xl transition-all ease-linear rounded-2xl flex flex-col overflow-hidden bg-white">
-          <div className="flex justify-between text-sm p-2">
-            <p className="bg- px-3 py-1 rounded-full"> Room No. {room.roomNumber}</p>
-            <p className="bg-black px-3 py-1 text-white rounded-full"> Type </p>
-          </div>
-          <div className="h-14 w-full">
-            <img
-              src={bg}
-              alt="background"
-              className="w-full h-14 object-cover object-center"
-            />
-          </div>
-          <div className="h-full px-2 pt-4 ">
-            <h1 className="text-2xl font-bold">{room.roomType}</h1>
-            <p className="text-xl font-semibold">{room.price}</p>
-          </div>
-        </div>
+      <div className="flex flex-col justify-center items-center md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 pb-3 overflow-auto">
+        {rooms.map((room) => {
+          return (
+            <div className="min-w-[15rem] max-w-[16rem] min-h-[12rem] max-h-[13rem] relative shadow-md hover:shadow-xl transition-all ease-linear rounded-2xl flex flex-col overflow-hidden bg-white">
+              <div className="flex justify-between text-sm p-2">
+                <p className="bg-gray-200 px-4 py-1 font-medium rounded-full">
+                  {room.roomNumber}
+                </p>
+                <p className="bg-black px-3 py-1 text-white rounded-full">
+                  Type
+                </p>
+              </div>
+              <div className="h-14 w-full">
+                <img
+                  src={bg}
+                  alt="background"
+                  className="w-full h-14 object-cover object-center"
+                />
+              </div>
+              <div className="h-full px-3 pt-4 pb-2 ">
+                <h1 className="text-2xl font-bold">{room.roomType}</h1>
+                <p className="text-xl font-semibold">₹{room.price}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
-        );
-      })}
-      
+
       <AnimatePresence>
         {isOpen && <Modal handleModal={handleModal} />}
       </AnimatePresence>

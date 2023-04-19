cbbc29d4c75d1f18661fc2fa1dcf552dfb0b1540
w-udhav <udhavwadhawan@hotmail.com>
2023-04-19 15:19:39 +0530

    new: cancellation page ui ready

--- src/Pages/Cancellation/Cancel/Cancel.js
+++ src/Pages/Cancellation/Cancel/Cancel.js
@@ -1,9 +1,73 @@
 import React from "react";
+import avatar from "../../../Assets/avatar.png";
 
 export default function Cancel() {
   return (
     <div className="w-full h-full">
-      <div className="flex justify-center items-center border"></div>
+      <div className="flex justify-center items-center">
+        <div className="min-w-[25rem] h-[max-content]  rounded-xl">
+          <div className="rounded-2xl bg-s1 shadow-xl">
+            <h1 className="text-white font-black text-4xl text-center px-2 py-5">
+              {" "}
+              RECIEPT{" "}
+            </h1>
+          </div>
+
+          {/* reciept */}
+          <div className="rounded-2xl shadow-xl w-full pt-10 -mt-1 py-3 px-2 flex flex-col gap-8">
+            {/* Refund Amount */}
+            <div className="flex flex-col items-center gap-2">
+              <h1 className="font-semibold text-3xl"> Rs. 1000 </h1>
+              <p className="text-md text-gray-400 pb-1">
+                {" "}
+                Room service & booking cost{" "}
+              </p>
+              <div className="bg-yellow-200 bg-opacity-40 rounded-xl text-[13px] px-4 py-1 text-yellow-600">
+                &#x2022; Pre-Booked
+              </div>
+            </div>
+
+            {/* User Detail */}
+            <div className="flex flex-col gap-4 px-2">
+              <div className="border-t border-gray-200"></div>
+
+              <div className="flex items-center gap-3">
+                <img src={avatar} alt="profile" className="w-14 rounded-full" />
+                <div className="flex flex-col gap-1">
+                  <h3 className="font-medium"> Udhav Wadhawan </h3>
+                  <p className="text-[14px] text-gray-500">
+                    {" "}
+                    ambrewster24@gmail.com{" "}
+                  </p>
+                </div>
+              </div>
+
+              <div className="border-t border-gray-200"></div>
+
+              {/* Checks */}
+              <div className="">
+                <h2 className="font-medium text-lg pb-1"> Checks </h2>
+                <div className="flex items-center gap-3">
+                  <h3 className="text-gray-500 w-14"> From: </h3>
+                  <p> Date </p>
+                </div>
+                <div className="flex items-center gap-3">
+                  <h3 className="text-gray-500 w-14 "> To: </h3>
+                  <p> Date </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Cancel button */}
+          <div className="py-8 flex flex-col gap-3">
+            <p> Are you sure? </p>
+            <button className="px-2 py-3 rounded-md shadow-md hover:shadow-xl transition-all ease-linear bg-red-500 text-white w-full">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
